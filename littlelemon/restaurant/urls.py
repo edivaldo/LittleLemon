d24f0e1e55eb2647bb23d6444560c9beb8d12e11
edwell <edivaldosilvalmeida@gmail.com>
2023-08-13 12:11:55 -0300

    api implementations

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+#from .views import menuview, bookingview
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home', views.index, name='home')
+    path('home', views.index, name='home'),
+    #path('menu/', menuview.as_view()),
+    #path('booking/', bookingview.as_view()),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,58 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, viewsets
+
+from .models import Menu, Booking
+from .serializers import menuSerializer, bookingSerializer
+
 
 def index(request):
     return render(request, 'restaurant/index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+    search_fields = ['category__title']
+    ordering_fields = ['price', 'inventory']
+
+class SingleMenuView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    
+"""
+class bookingview(APIView):
+    
+    def get(self,request):
+        items = Booking.objects.all()
+        serializer = bookingSerializer(items, many = True)
+        return Response(serializer.data) # return Json
+    
+    def post(self,request):
+        serializer = bookingSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':'success', 'data': serializer.data})
+        
+
+
+class menuview(APIView):
+    
+    def get(self,request):
+        items = Menu.objects.all()
+        serializer = menuSerializer(items, many = True)
+        return Response(serializer.data) # return Json
+    
+    def post(self,request):
+        serializer = menuSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':'success', 'data': serializer.data})
+"""
+
